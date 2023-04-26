73427f6ee6a61e0e0ae2c5d77e16009ac0f4e522
djametcode <kampretsiji1@gmail.com>
2023-04-26 14:40:21 +0700

    changes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use("/api/v8/kawan-virtual/", auth, userRoute);
 const startServer = async () => {
   try {
     await connectDB(MONGO_URL);
-    app.listen(port, console.log(`server running ....`));
+    app.listen(console.log(`server running ....`));
   } catch (error) {
     console.log(error);
   }
