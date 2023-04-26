60df9d8ed7a5041f4066f533254d313ec700afd4
djametcode <kampretsiji1@gmail.com>
2023-04-26 12:55:51 +0700

    changes

--- frontend/kawanonline/pages/profile.jsx
+++ frontend/kawanonline/pages/profile.jsx
@@ -1,8 +1,14 @@
 import Header from "@/components/Header";
 import ProfileComponent from "@/components/ProfileComponent";
 import Navigation from "@/components/navigation";
+import { useRouter } from "next/router";
 
 export default function Profile() {
+  const route = useRouter();
+  const handleLogOut = () => {
+    localStorage.clear();
+    route.push("/login");
+  };
   return (
     <div>
       <div>
@@ -10,6 +16,9 @@ export default function Profile() {
       </div>
       <div>
         <ProfileComponent />
+      </div>
+      <div>
+        <button onClick={handleLogOut}>LogOut</button>
       </div>
       <div className=" fixed bottom-0 w-screen">
         <Navigation />
